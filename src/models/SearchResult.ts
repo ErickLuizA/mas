61795a54d506686597952e3a5b0bc60ac0267ff0
erickLuizA <erickluiz69@gmail.com>
2021-07-20 23:29:50 -0300

    feat: models

---
+++ src/models/SearchResult.ts
@@ -0,0 +1,16 @@
+export interface SearchResult {
+  poster_path: any
+  popularity: number
+  id: number
+  overview: string
+  backdrop_path: any
+  vote_average: number
+  media_type: string
+  first_air_date: string
+  origin_country: string[]
+  genre_ids: any[]
+  original_language: string
+  vote_count: number
+  name: string
+  original_name: string
+}
