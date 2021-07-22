33955c95f9053f896bb30a0a6eb233d2ea5e2bb7
erickLuizA <erickluiz69@gmail.com>
2021-07-22 16:59:08 -0300

    refactor: Header component in navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,43 +1,34 @@
 import React from 'react'
-import { View, Image, StyleSheet } from 'react-native'
+import { Image, StyleSheet } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 
 import icon from '../images/icon.png'
 import { RectButton } from 'react-native-gesture-handler'
 import { useNavigation } from '@react-navigation/native'
 import StyleGuide from './StyleGuide'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export default function Header() {
+  const { navigate } = useNavigation()
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image source={icon} />
+      <RectButton
+        rippleColor={StyleGuide.primary}
+        onPress={() => navigate('Search')}>
+        <MaterialIcons name="search" size={30} color="#ddd" />
+      </RectButton>
+    </SafeAreaView>
+  )
+}
 
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    flexGrow: 0.05,
-  },
-
-  img: {
-    resizeMode: 'contain',
-  },
-
-  button: {
-    paddingHorizontal: 10,
-    paddingVertical: 10,
-    top: 10,
+    backgroundColor: StyleGuide.background,
+    paddingHorizontal: 12,
   },
 })
-
-export default function Header() {
-  const { navigate } = useNavigation()
-
-  return (
-    <View style={styles.container}>
-      <Image source={icon} style={styles.img} />
-      <RectButton
-        rippleColor={StyleGuide.primary}
-        style={styles.button}
-        onPress={() => navigate('Search')}>
-        <MaterialIcons name="search" size={30} color="#fff" />
-      </RectButton>
-    </View>
-  )
-}
